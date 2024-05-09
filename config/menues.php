d9307753e715e1f3e8231853ede61342021a96fb
davide-piarulli <davide.piarulli.1@gmail.com>
2024-05-09 16:09:22 +0100

    [add] array di array

---
+++ config/menues.php
@@ -0,0 +1,81 @@
+<?php
+return [
+    'headerMenues' => [
+        'navLeft' => [
+            [
+                'genere' => 'Donna',
+                'link' => '#'
+            ],
+            [
+                'genere' => 'Uomo',
+                'link' => '#'
+            ],
+            [
+                'genere' => 'Bambini',
+                'link' => '#'
+            ],
+        ],
+        'navRight' => [
+            [
+                'icon' => '<i class="fa-regular fa-user"></i>',
+                'link' => '#'
+            ],
+            [
+                'icon' => '<i class="fa-regular fa-heart"></i>',
+                'link' => '#'
+            ],
+            [
+                'icon' => '<i class="fa-solid fa-bag-shopping"></i>',
+                'link' => '#'
+            ],
+        ],
+        'img' => [
+            'path' => 'boolean-logo.png',
+            'title' => 'logo'
+        ]
+    ],
+
+    'footerMenues' => [
+        'lawPages' => [
+            [
+                'name' => 'Informazioni legali',
+                'link' => '#'
+            ],
+            [
+                'name' => 'Informazioni sulla privacy',
+                'link' => '#'
+            ],
+            [
+                'name' => 'Diritto di recesso',
+                'link' => '#'
+            ],
+        ],
+
+        'icons' => [
+            [
+                'img' => '<i class="fa-brands fa-square-twitter"></i>',
+                'link' => 'https=>//twitter.com/'
+            ],
+
+            [
+                'img' => '<i class="fa-brands fa-square-facebook"></i>',
+                'link' => 'https=>//www.facebook.com/'
+            ],
+
+            [
+                'img' => '<i class="fa-brands fa-instagram"></i>',
+                'link' => 'https=>//www.instagram.com/'
+            ],
+
+            [
+                'img' => '<i class="fa-brands fa-square-pinterest"></i>',
+                'link' => 'https=>//www.pinterest.it/'
+            ],
+
+            [
+                'img' => '<i class="fa-brands fa-square-youtube"></i>',
+                'link' => 'https=>//www.youtube.com/'
+            ],
+        ]
+    ]
+];
--- resources/views/uomo.blade.php
+++ resources/views/uomo.blade.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <h1>Donna</h1>
+    <h1>Uomo</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis tempore, voluptatem at temporibus dignissimos in
         culpa porro voluptatum omnis libero aliquid, autem similique provident quia qui eos vitae quidem cupiditate
         minus voluptatibus sapiente fugit mollitia nemo. Sint quas, consequatur blanditiis qui ipsam necessitatibus
